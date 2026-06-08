4de00557d1388f638593f1c4710ebdab205f660a
bornface Kangombe <bornface.kangombe@cs.unza.zm>
2026-06-08 13:31:55 +0200

    Implement RootLayout with metadata and Player component
    Added metadata and layout structure for the app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,37 @@
 
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Player from "@/components/Player";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kwimbo Vibes",
+  description: "Stream music and videos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
+      >
+        {children}
+        <Player />
+      </body>
+    </html>
+  );
+}
